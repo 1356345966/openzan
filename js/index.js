9146b4d2deb105d8bda10988d18eb85d46927a40
hushiying <1356345966.qq.com>
2017-11-16 16:34:23 +0800

    胡世莹

--- js/index.js
+++ js/index.js
@@ -1,70 +1,47 @@
-angular.module('myApp', ['ionic'])
-.config(function($stateProvider, $urlRouterProvider) {
-	$stateProvider
-		.state('tab', {
-			url: "/tab",
-			abstract: true,
-			templateUrl: "templates/tabs.html"
-		})
-		.state('tab.MyCard', {
-			url: '/MyCard',
-			views: {
-				'tab-MyCard': {
-					templateUrl: "templates/tab-MyCard.html"
+(function(){
+	var m1=angular.module('myApp', ['ionic'])
+	m1.config(function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('tab', {
+				url: "/tab",
+				abstract: true,
+				templateUrl: "templates/tabs.html"
+			})
+			.state('tab.goods', {
+				url: '/goods',
+				views: {
+					'tab-goods': {
+						templateUrl: "templates/goods.html"
+					}
 				}
-			}
-		})
-		.state("tab.tab-MyCard-sender",{
-			url:"/tab-MyCard-sender",
-			views: {
-				'tab-MyCard': {
-					templateUrl: "templates/tab-MyCard-sender.html"
+			})
+			.state('tab.CardHolder', {
+				url: '/CardHolder',
+				views: {
+					'tab-CardHolder': {
+						templateUrl: "templates/tab-CardHolder.html"
+					}
 				}
-			}
-		})
-		.state("tab.tab-MyCard-senderDetail",{
-			url:"/tab-MyCard-senderDetail",
-			views: {
-				'tab-MyCard': {
-					templateUrl: "templates/tab-MyCard-senderDetail.html"
+			})
+			.state('tab.Circle', {
+				url: '/Circle',
+				views: {
+					'tab-Circle': {
+						templateUrl: "templates/tab-Circle.html"
+					}
 				}
-			}
-		})
-		.state('tab.CardHolder', {
-			url: '/CardHolder',
-			views: {
-				'tab-CardHolder': {
-					templateUrl: "templates/tab-CardHolder.html"
+			})
+			.state('tab.More', {
+				url: '/More',
+				views: {
+					'tab-More': {
+						templateUrl: "templates/tab-More.html"
+					}
 				}
-			}
-		})
-		.state('tab.Circle', {
-			url: '/Circle',
-			views: {
-				'tab-Circle': {
-					templateUrl: "templates/tab-Circle.html"
-				}
-			}
-		})
-		.state('tab.More', {
-			url: '/More',
-			views: {
-				'tab-More': {
-					templateUrl: "templates/tab-More.html"
-				}
-			}
-		})
-		.state('tab.edit', {
-			url: '/edit/:id',
-			views: {
-				'tab-MyCard': {
-					templateUrl: "templates/tab-edit.html"
-				}
-			}
-		})
-		.state('news', {
-			url: '/news',
-			templateUrl: "templates/news.html"
-		})
-	$urlRouterProvider.otherwise('/tab/MyCard');
-});
+			})
+		$urlRouterProvider.otherwise('/tab/goods');
+		
+	});
+	
+
+})()
